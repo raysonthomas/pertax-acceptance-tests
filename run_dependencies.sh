fe7fad371fa8db263e5c296683240480430f000f
Nic Fellows <nic@nicshouse.co.uk>
2015-01-19 16:11:44 +0000

    Move test dependencies to shell script

---
+++ run_dependencies.sh
@@ -0,0 +1,11 @@
+export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_51
+
+sm --config /etc/smserver/conf --start PERTAX_ALL -f --wait 90 --noprogress
+
+if pidof BrowserStackLocal; then
+  echo "BrowserStackLocal running already"
+else
+  wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+  unzip BrowserStackLocal-linux-x64.zip
+  ./BrowserStackLocal Sauyv85GKXmkxFwAxbmY
+fi
