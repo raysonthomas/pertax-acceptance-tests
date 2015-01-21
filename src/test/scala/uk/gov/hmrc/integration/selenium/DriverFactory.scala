116821552f26bf64795bd31f316363550e8e95ba
Nic Fellows <nic@nicshouse.co.uk>
2015-01-21 14:14:17 +0000

    Remove unnecessary indirection

--- src/test/scala/uk/gov/hmrc/integration/selenium/DriverFactory.scala
+++ src/test/scala/uk/gov/hmrc/integration/selenium/DriverFactory.scala
@@ -13,21 +13,21 @@ object DriverFactory {
 
   def buildWebDriver = {
     val webDriver = System.getProperty("browser", "firefox-local") match {
-      case "firefox-local"        => createLocalFirefoxDriver
-      case "chrome-local"         => createLocalChromeDriver
-      case "winxp-ie6-remote"     => createRemoteWinXPie6Driver
-      case "winxp-ie7-remote"     => createRemoteWinXPie7Driver
-      case "win7-ie8-remote"      => createRemoteWin7ie8Driver
-      case "win7-ie9-remote"      => createRemoteWin7ie9Driver
-      case "win7-ie10-remote"     => createRemoteWin7ie10Driver
-      case "win7-ie11-remote"     => createRemoteWin7ie11Driver
-      case "win8-ie11-remote"     => createRemoteWin8ie10Driver
-      case "win8.1-ie11-remote"   => createRemoteWin81ie11Driver
-      case "macos-safari7-remote" => createRemoteSafar7Driver
-      case "macos-safari8-remote" => createRemoteSafar8Driver
-      case "macos-chrome-remote"  => createRemoteChrome38river
-      case "macos-firefox-remote" => createRemoteFirefox33Driver
-      case "iphone5s-remote"      => createRemoteBuildIphone5SDriver
+      case "firefox-local"        => new FirefoxDriver
+      case "chrome-local"         => new ChromeDriver
+      case "winxp-ie6-remote"     => buildWinXPie6Driver
+      case "winxp-ie7-remote"     => buildWinXPie7Driver
+      case "win7-ie8-remote"      => buildWin7ie8Driver
+      case "win7-ie9-remote"      => buildWin7ie9Driver
+      case "win7-ie10-remote"     => buildWin7ie10Driver
+      case "win7-ie11-remote"     => buildWin7ie11Driver
+      case "win8-ie11-remote"     => buildWin8ie10Driver
+      case "win8.1-ie11-remote"   => buildWin81ie11Driver
+      case "macos-safari7-remote" => buildMacosxMavericksSafari7Driver
+      case "macos-safari8-remote" => buildMacosxYosemitesSafari8Driver
+      case "macos-chrome-remote"  => buildMacosxYosemitechrome38Driver
+      case "macos-firefox-remote" => buildMacosxYosemitefirefox33Driver
+      case "iphone5s-remote"      => buildIosIphone5SDriver
       case _ => throw new IllegalArgumentException(s"Browser type not recognised")
     }
 
@@ -42,35 +42,5 @@ object DriverFactory {
     
     webDriver
   }
-
-  //Local Drivers
-  def createLocalFirefoxDriver = new FirefoxDriver
-  def createLocalChromeDriver = new ChromeDriver
-
-  //Aliases for the cross browser testing  
-  //Aliases-WIN-XP for the cross browser testing
-  def createRemoteWinXPie7Driver  = buildWinXPie7Driver
-  def createRemoteWinXPie6Driver  = buildWinXPie6Driver
-
-  //Aliases-WIN-7 for the cross browser testing
-  def createRemoteWin7ie8Driver = buildWin7ie8Driver
-  def createRemoteWin7ie9Driver = buildWin7ie9Driver
-  def createRemoteWin7ie10Driver = buildWin7ie10Driver
-  def createRemoteWin7ie11Driver = buildWin7ie11Driver
-
-  //Aliases-WIN-8 for the cross browser testing
-  def createRemoteWin8ie10Driver = buildWin8ie10Driver
-  
-  //Aliases-WIN-8.1 for the cross browser testing
-  def createRemoteWin81ie11Driver = buildWin81ie11Driver
-   
-  //Aliases-MAC for the cross browser testing
-  def createRemoteSafar7Driver    = buildMacosxMavericksSafari7Driver
-  def createRemoteSafar8Driver    = buildMacosxYosemitesSafari8Driver
-  def createRemoteChrome38river    = buildMacosxYosemitechrome38Driver
-  def createRemoteFirefox33Driver  = buildMacosxYosemitefirefox33Driver
-  
-  //Aliases-IOS Device for the cross browser testing
-  def createRemoteBuildIphone5SDriver  = buildIosIphone5SDriver
 }
 
