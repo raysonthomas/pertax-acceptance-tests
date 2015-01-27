c3139575c751bad0018fa494c6a01ae2a331ef0e
Sriman Mittapally <sriman.mittapally@accenture.com>
2015-01-27 14:13:24 +0000

    Updated Globalactions

--- src/test/scala/uk/gov/hmrc/integration/page/GlobalActions.scala
+++ src/test/scala/uk/gov/hmrc/integration/page/GlobalActions.scala
@@ -1,13 +1,13 @@
 package uk.gov.hmrc.integration.page
 
-import org.openqa.selenium.{WebDriver, By}
-import uk.gov.hmrc.integration.utils.Configuration
+import org.openqa.selenium.{By, WebDriver}
+
 
 object GlobalActions {
 
   def maybeClickMenu(implicit webDriver: WebDriver) = {
     val dims = webDriver.findElement(By.cssSelector("body")).getSize
-    if (dims.width <= Configuration.DeviceViewBreakpoint)
+    if (dims.width <= uk.gov.hmrc.integration.utils.Configuration.DeviceViewBreakpoint)
       webDriver.findElement(By.linkText("Menu")).click()
   }
 }
