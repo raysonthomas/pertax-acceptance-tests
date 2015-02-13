d68d3aac60155d0c8c4a0ed001b88c0bafda3ce0
Sriman Mittapally <sriman.mittapally@accenture.com>
2015-02-13 15:38:24 +0000

    Keep BrowserStackLocal in /tmp

--- run_dependencies.sh
+++ run_dependencies.sh
@@ -6,10 +6,14 @@ sm --config /etc/smserver/conf --start PERTAX_ALL TAI TAI_STUBS -f --wait 90 --n
 echo "BrowserStackLocal instances:"
 pidof BrowserStackLocal
 
+cd /tmp
+
 if pidof BrowserStackLocal; then
   echo "BrowserStackLocal running already"
 else
-  wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
-  unzip BrowserStackLocal-linux-x64.zip
+  if [ ! -e BrowserStackLocal ]; then
+    wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+    unzip BrowserStackLocal-linux-x64.zip
+  fi
   ./BrowserStackLocal Sauyv85GKXmkxFwAxbmY &
 fi
