1c66eb97edb23982b025f211a2ff5da139f39c35
Prasad Sriramula <prasad.sriramula@digital.hmrc.gov.uk>
2015-05-19 12:49:10 +0100

    MTA-497 Remove beta banner features, covered by unit tests now

--- run_integration_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sbt -Dbrowser=chrome -Denvironment=dev 'test-only uk.gov.hmrc.integration.cucumber.RunDevTestSuite'
--- run_integration_local.sh
+++ run_integration_local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbt -Dbrowser=firefox-local -Denvironment=local 'test-only uk.gov.hmrc.integration.suites.RunLocalTestSuite'
+sbt -Dbrowser=firefox-local -Denvironment=local 'test-only uk.gov.hmrc.integration.cucumber.RunTestSuiteAgent'
