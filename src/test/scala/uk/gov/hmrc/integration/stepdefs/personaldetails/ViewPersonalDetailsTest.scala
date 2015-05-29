233632b672403a0bf40c3fb606ec7f15dd3aeb92
Nic Fellows <nic@nicshouse.co.uk>
2015-05-29 12:32:29 +0100

    Consolidate personal details journey tests

--- src/test/scala/uk/gov/hmrc/integration/stepdefs/personaldetails/ViewPersonalDetailsTest.scala
+++ src/test/scala/uk/gov/hmrc/integration/stepdefs/personaldetails/ViewPersonalDetailsTest.scala
@@ -5,7 +5,25 @@ import org.openqa.selenium.By
 import uk.gov.hmrc.integration.selenium.CurrentDriver._
 import scala.collection.JavaConversions._
 
-class ViewAddressTest extends ScalaDsl with EN {
+class ViewPersonalDetailsTest extends ScalaDsl with EN {
+
+  Then( """^Name '(.*)' is visible$""") { (name: String) =>
+    withCurrentDriver { implicit webDriver =>
+      assert(
+        webDriver.findElements(By.cssSelector(".no-margin-top")).filter(_.getText == name).isEmpty == false,
+        s"$name was not found in '.no-margin-top' field"
+      )
+    }
+  }
+
+  Then( """^NINO '(.*)' is visible$""") { (nino: String) =>
+    withCurrentDriver { implicit webDriver =>
+      assert(
+        webDriver.findElements(By.cssSelector(".no-margin-top")).filter(_.getText == nino).isEmpty == false,
+        s"$nino was not found in '.no-margin-top' field"
+      )
+    }
+  }
 
   Then( """^Current Address '(.*)', '(.*)', '(.*)', '(.*)', '(.*)' should be visible$""") {
     (addressLine1: String, addressLine2: String, addressLine3: String, addressLine4: String, postCode: String) =>
