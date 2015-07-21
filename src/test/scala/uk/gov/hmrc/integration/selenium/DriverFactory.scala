80e781bcbebe7f16af9dab39a5663eaff428fe9e
Prasad Sriramula <prasad.sriramula@digital.hmrc.gov.uk>
2015-07-21 13:21:48 +0100

    Added whitelist parameter to the accessibility driver

--- src/test/scala/uk/gov/hmrc/integration/selenium/DriverFactory.scala
+++ src/test/scala/uk/gov/hmrc/integration/selenium/DriverFactory.scala
@@ -17,7 +17,7 @@ object DriverFactory {
     val webDriver = System.getProperty("browser", "firefox-local") match {
       case "firefox-local"            => new FirefoxDriver
       case "chrome-local"             => new ChromeDriver
-      case "accessibility"            => new AccessibilityDriver(23748)
+      case "accessibility"            => new AccessibilityDriver(23748,List("^http://localhost:9232/personal-account(.*)".r))
       case "winxp-ie6-remote"         => buildWinXPie6Driver
       case "winxp-ie7-remote"         => buildWinXPie7Driver
       case "win7-ie8-remote"          => buildWin7ie8Driver
