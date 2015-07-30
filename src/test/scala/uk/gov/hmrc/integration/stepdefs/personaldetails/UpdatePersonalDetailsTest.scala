07c879f9eae57737b6d4e46d9cdff4e6e1392e78
kristapsmelderis <kmelderis0010@gmail.com>
2015-07-30 03:27:56 -0700

    MTA-715 add updateAddress check stepdef

--- src/test/scala/uk/gov/hmrc/integration/stepdefs/personaldetails/UpdatePersonalDetailsTest.scala
+++ src/test/scala/uk/gov/hmrc/integration/stepdefs/personaldetails/UpdatePersonalDetailsTest.scala
@@ -17,4 +17,17 @@ class UpdatePersonalDetailsTest extends ScalaDsl with EN {
     }
   }
 
+  // this step definition does nothing if it is not qa environment (because we cant modify address in stubs)
+  Then( """^Updated Address '(.*)', '(.*)', '(.*)', '(.*)', '(.*)' should be visible$""") {
+    (addressLine1: String, addressLine2: String, addressLine3: String, addressLine4: String, postCode: String) =>
+      withCurrentDriver { implicit webDriver =>
+        if(System.getProperty("environment").toLowerCase == "qa") {
+          val addressElements = Array(addressLine1, addressLine2, addressLine3, addressLine4, postCode).filter(_ != "")
+          val expectedAddress = addressElements.mkString("\n")
+          val actualAddress = webDriver.findElement(By.cssSelector("address")).getText
+          assert(expectedAddress == actualAddress, s"$expectedAddress did not match $actualAddress")
+        }
+      }
+  }
+
 }
