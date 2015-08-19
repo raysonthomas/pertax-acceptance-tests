1c79f14bdee51822d25d2ab913ecd909b47f7fc1
kristapsmelderis <kmelderis0010@gmail.com>
2015-08-19 09:25:52 -0700

    fix link to tai test (url endsWith)

--- src/test/scala/uk/gov/hmrc/integration/stepdefs/home/AnnualTaxableIncomeTest.scala
+++ src/test/scala/uk/gov/hmrc/integration/stepdefs/home/AnnualTaxableIncomeTest.scala
@@ -38,7 +38,7 @@ class AnnualTaxableIncomeTest extends ScalaDsl with EN {
   And( """^the 'View details' link leads to the TAI landing page$""") {
     () =>
       withCurrentDriver { implicit webDriver =>
-        val targetURL = "/tai/landing-page"
+        val targetURL = "/check-income-tax/income-tax"
         assert(webDriver.findElements(By.cssSelector(".income-estimate a")).filter(_.getAttribute("href").contains(targetURL)).nonEmpty,
           s"the 'View details' link leads to the TAI tax estimate page URL '$targetURL' was not found in '.income-estimate a' field"
         )
@@ -48,7 +48,7 @@ class AnnualTaxableIncomeTest extends ScalaDsl with EN {
   And( """^Tax you'll pay link leads to the TAI landing page$""") {
     () =>
       withCurrentDriver { implicit webDriver =>
-        val targetURL = "/tai/landing-page"
+        val targetURL = "/check-income-tax/income-tax"
         assert(webDriver.findElements(By.cssSelector(".tax-estimate a")).filter(_.getAttribute("href").contains(targetURL)).nonEmpty,
           s"Tax you'll pay link leads to the TAI tax you'll pay page URL '$targetURL' was not found in '.tax-estimate a' field"
         )
