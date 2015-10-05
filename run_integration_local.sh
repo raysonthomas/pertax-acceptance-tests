3016f70e00fa4a347f1fc237feafd0b642ebb238
kristapsmelderis <kristaps.melderis@digital.hmrc.gov.uk>
2015-10-05 08:20:08 -0700

    added attorney.drop to clear trusted helper relations before tests are run

--- run_integration_local.sh
+++ run_integration_local.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+{
+sleep 1
+echo "use attorney"
+echo "db.attorney.drop()"
+} | mongo
+
 sbt -Dbrowser=firefox-local -Denvironment=local 'test-only uk.gov.hmrc.integration.cucumber.RunTestSuiteAgent'
