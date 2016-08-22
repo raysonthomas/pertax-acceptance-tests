1a7c543a74bf4423fa60b5d08bef35e971c83c32
raysonthomas <rayson.thomas@digital.hmrc.gov.uk>
2016-08-22 14:50:35 +0100

    MTA-2218 auth-login-steps

--- src/test/scala/uk/gov/hmrc/integration/stepdefs/home/SAActivation.scala
+++ src/test/scala/uk/gov/hmrc/integration/stepdefs/home/SAActivation.scala
@@ -1,6 +1,7 @@
 package uk.gov.hmrc.integration.stepdefs.home
 
 import cucumber.api.scala.{EN, ScalaDsl}
+import uk.gov.hmrc.integration.page.AuthActions
 import uk.gov.hmrc.integration.selenium.CurrentDriver._
 
 class SAActivation extends ScalaDsl with EN {
@@ -8,6 +9,12 @@ class SAActivation extends ScalaDsl with EN {
   And( """^user sees the text '(.*)' on SA page$""") {
     (expectedText: String) => withCurrentDriver { implicit webDriver =>
       assert(webDriver.getPageSource.contains(expectedText), s"\n'$expectedText' text was not found on the page")
+    }
+  }
+
+  Given( """^(.*) logs into the service for SA using (.*)$""") { (user: String, authProvider: String) =>
+    withCurrentDriver { implicit webDriver =>
+      AuthActions.logInforSA(user, authProvider)
     }
   }
 
